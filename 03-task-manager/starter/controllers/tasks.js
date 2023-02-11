42f1809410ac3ab067138230105345c2d86ba7d1
georgeowusu <gkojunior@gmail.com>
2023-02-11 08:56:41 -0500

    Merge branch 'main' into week5

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,31 +1,47 @@
-const Task = require('../models/Task')
+const Task = require('../models/Task');
 
-const getAllTasks = (req, res) => {
-    res.send('get all tasks')
-}
+const getAllTasks = async (req, res) => {
+	try {
+		const tasks = await Task.find({});
+		res.status(200).json({ tasks });
+	} catch (error) {
+		res.status(500).json({ msg: error });
+	}
+};
 const createTask = async (req, res) => {
-    try {
-        const task = await Task.create(req.body)
-        res.status(201).json({task})
-    } catch (error){
-        res.status(500).json({msg:error})
-    }
+	try {
+		const task = await Task.create(req.body);
+		res.status(201).json({ task });
+		console.log(req.body);
+	} catch (error) {
+		res.status(500).json({ msg: error });
+	}
+};
+const getTask = async (req, res) => {
+	try {
+		const { id: taskID } = req.params;
+		const task = await Task.findOne({ _id: taskID });
+		if (!task) {
+			return res.status(404).json({ msg: `No task with id : ${taskID}` });
+		}
 
-}
-const getTask = (req, res) => {
-    res.json({id:req.params.id})
-}
+		res.status(200).json({ task });
+	} catch (error) {
+		res.status(500).json({ msg: error });
+	}
+};
+
 const updateTask = (req, res) => {
-    res.send('update task')
-}
+	res.send('update task');
+};
 const deleteTask = (req, res) => {
-    res.send('delete task')
-}
+	res.send('delete task');
+};
 
 module.exports = {
-    getAllTasks,
-    createTask,
-    getTask,
-    updateTask,
-    deleteTask
-}
+	getAllTasks,
+	createTask,
+	getTask,
+	updateTask,
+	deleteTask,
+};
