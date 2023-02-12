ec1e0cbf8ff5bb43dadefa92758872e110cd9eae
georgeowusu <gkojunior@gmail.com>
2023-02-12 12:24:52 -0500

    Week 6:Task Manager API Part 2

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -3,21 +3,21 @@ const app = express();
 const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect');
 require('dotenv').config();
-// const notFound = require('./middleware/not-found');
-// const errorHandlerMiddleware = require('./middleware/error-handler');
+const notFound = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
 
 // middleware
 
-// app.use(express.static('./public'));
+app.use(express.static('./public'));
 app.use(express.json());
 
 // routes
-
 app.use('/api/v1/tasks', tasks);
 
-// app.use(notFound);
-// app.use(errorHandlerMiddleware);
-const port = 3000;
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.Port || 3000;
 
 const start = async () => {
   try {
