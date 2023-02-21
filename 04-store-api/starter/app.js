9bf0cff6f29d2c850d9c79b0e2bf68cf0956e410
georgeowusu <gkojunior@gmail.com>
2023-02-20 22:26:30 -0500

    Week 7: Using Query Parameters

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,10 +1,11 @@
-require('dotenv').config();
-// async errors
+require('dotenv').config()
+require('express-async-errors')
 
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
 const connectDB = require('./db/connect')
+const productsRouter = require('./routes/products')
 
 const notFoundMiddleware = require('./middleware/not-found')
 const errorMiddleware = require('./middleware/error-handler')
@@ -15,10 +16,10 @@ app.use(express.json())
 // rootes
 
 app.get('/', (req, res) => {
-    res.send('<h1>Store API</h1><a href="/api/v1/products"> products route</a>')
+	res.send('<h1>Store API</h1><a href="/api/v1/products"> products route</a>')
 })
 
-app.use('/api/v1/products')
+app.use('/api/v1/products', productsRouter)
 
 // products route
 
@@ -28,12 +29,12 @@ app.use(errorMiddleware)
 const port = process.env.Port || 3000
 
 const start = async () => {
-    try {
-        await connectDB(process.env.MONGO_URI)
-        app.listen(port, console.log(`Server is listening on port ${port}...`))
-    } catch (error) {
-        console.log(error);
-    }
+	try {
+		await connectDB(process.env.MONGO_URI)
+		app.listen(port, console.log(`Server is listening on port ${port}...`))
+	} catch (error) {
+		console.log(error)
+	}
 }
 
 start()
